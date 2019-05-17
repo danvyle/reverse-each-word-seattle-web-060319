482077a28e567b017bc39ff4090923214fad6b9f
Dan-Vy Le <danvyle85@gmail.com>
2019-05-17 22:42:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,9 +5,3 @@ reversed_sentence = sentence.split(" ").collect do |x|
     reversed_sentence.join(" ")
 end
 
-# def reverse_each_word(sentence)
-#   reversed = sentence.split(" ").collect do |word|
-#   word.reverse
-# end
-#   reversed.join(" ")
-# end
