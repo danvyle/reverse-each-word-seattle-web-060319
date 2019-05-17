08a907720536bb8328152eee6d5f2de3a1206cac
Dan-Vy Le <danvyle85@gmail.com>
2019-05-17 22:40:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def change_nums(num)
-new_num = num.split(" ").collect do |x| 
-    x.reverse
-   end
-    new_num.join(" ")
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
 end
